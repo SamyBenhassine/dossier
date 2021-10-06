51c12911df7e6baaaaf8440cd536f428a2873f7c
Samy <samybenhassine19@gmail.com>
2021-10-06 14:59:35 +0200

    family value added

--- fichiertest.java
+++ fichiertest.java
@@ -4,8 +4,7 @@ public class essai {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		System.out.println("quel est votre age?");
-		System.out.println("quel est votre nom");
-		
+		System.out.println("avez vous une famille?)";
 	}
 
 }
