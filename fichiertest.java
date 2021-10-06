07ec4460ac8ff859c1fcf87d4cade08d466825b5
Samy <samybenhassine19@gmail.com>
2021-10-06 12:27:30 +0200

    new age parameter

--- fichiertest.java
+++ fichiertest.java
@@ -3,7 +3,7 @@ public class essai {
 
 	public static void main(String[] args) {
 		System.out.println("Hello git");
-		System.out.println("fichier essai sur java");
+		System.out.println("quel est votre age?");
 		
 	}
 
