5da79494fc437044b9cc595341a748b32c37f187
Samy <samybenhassine19@gmail.com>
2021-10-06 15:03:36 +0200

    height value added

--- fichiertest.java
+++ fichiertest.java
@@ -4,8 +4,8 @@ public class essai {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		System.out.println("quel est votre age?");
-		System.out.println("quel est votre nom");
-		
+		System.out.println("avez vous une famille?)";
+		System.out.println("quelle taille faites vous?");
 	}
 
 }
